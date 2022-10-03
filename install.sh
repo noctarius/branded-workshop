fc370dc911185e0ac0b0ed6a85cf6c1ae7fe0d1e
noctarius aka Christoph Engelbert <me@noctarius.com>
2022-10-03 17:50:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 sudo sh -c 'echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" > /etc/apt/sources.list.d/timescaledb.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
-sudo apt -y update && apt install git postgresql-14 timescaledb-2-2.8.0-postgresql-14 timescaledb-tools timescaledb-toolkit-postgresql-14 grafana mosquito
+sudo apt -y update && apt install git postgresql-14 timescaledb-2-2.8.0-postgresql-14 timescaledb-tools timescaledb-toolkit-postgresql-14 grafana mosquitto
 sudo timescaledb-tune  --quiet --yes
 sudo bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --skip-pi
 sudo mkdir -p /root/.node-red
