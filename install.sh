f9580348ccec5bd9516f9488ce9f16b1cc04a60e
noctarius aka Christoph Engelbert <me@noctarius.com>
2022-10-03 18:28:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo ap
 sudo apt update
 sudo apt -y install git postgresql-14 timescaledb-2-2.8.0-postgresql-14 timescaledb-tools timescaledb-toolkit-postgresql-14 grafana mosquitto
 sudo timescaledb-tune  --quiet --yes
-sudo bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --skip-pi
+sudo bash -c 'bash <(curl -sL "https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered") --confirm-root --confirm-install --skip-pi'
 sudo mkdir -p /root/.node-red
 sudo wget https://raw.githubusercontent.com/noctarius/branded-workshop/main/settings.js -O /root/.node-red/settings.js
 sudo /bin/systemctl daemon-reload
