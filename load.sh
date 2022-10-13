68927abfaa5ba9b2c5a18578c07c15392875ee95
noctarius aka Christoph Engelbert <me@noctarius.com>
2022-10-13 13:20:38 +0200

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo wget https://github.com/noctarius/branded-workshop/raw/main/export-workshop.csv.tar.gz
-tar xvfz export-workshop.csv.tar.gz
-mv tmp/export-workshop.csv .
-rm -rf tmp
+sudo tar xvfz export-workshop.csv.tar.gz
+sudo mv tmp/export-workshop.csv .
+sudo  rm -rf tmp
 sudo -u postgres psql -c "alter user postgres password 'test1234'"
 sudo -u postgres psql -c "create table metrics(created timestamp with time zone default now() not null, type_id integer not null, value double precision not null);"
 echo "Importing data into database..."
